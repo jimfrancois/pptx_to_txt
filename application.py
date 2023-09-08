8f2c6c95a18dac7fa63ef52f5b43142713dd1dd3
jimfrancois <jimitryfrancois98@gmail.com>
2023-09-08 14:34:57 -0400

    This is the second version of the program

--- application.py
+++ application.py
@@ -1,25 +1,60 @@
 import comtypes.client
+import os
+import shutil
 
-def extract_text_from_powerpoint(input_path, output_path):
-    powerpoint = comtypes.client.CreateObject("PowerPoint.Application")
-    presentation = powerpoint.Presentations.Open(input_path)
-    
-    text = []
+# ---------------------------------------|| Version 2.0 ||--------------------------------------------------
+# Note: No error, Suceeded, but the txt files are in the same folder
+# ---------------------------------------|| Version 2.0 ||--------------------------------------------------
 
-    for slide in presentation.Slides:
-        for shape in slide.Shapes:
-            if shape.HasTextFrame:
-                text.append(shape.TextFrame.TextRange.Text)
-    
-    presentation.Close()
-    powerpoint.Quit()
 
-    with open(output_path, "w", encoding="utf-8") as txt_file:
-        txt_file.write("\n".join(text))
+# Define the folder path containing the PowerPoint files
 
-if __name__ == "__main__":
-    input_powerpoint = "C:\\Users\\Dell\\OneDrive\\Desktop\\pptx_to_txt\\weeek-end  27 aout 2023.pptx"  # Replace with your input PowerPoint file
-    output_text = "output.txt"       # Replace with the desired output text file
+# folder_path = 'C:\\Users\\HP\\Desktop\\samTech\\Projects\\Back-End\\Python\\PowerPoint_to_txt_script\\Powerpoint_to_txt\\All_pp_files'
+folder_path = 'C:\\Users\\Dell\\OneDrive\\Desktop\\pptx_to_txt\\Input_pptx'
 
-    extract_text_from_powerpoint(input_powerpoint, output_text)
-    print("Text extracted and saved to", output_text)
+output_path = ''
+
+# Initialize the PowerPoint application
+powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
+
+# Iterate through files in the folder
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        if file.endswith('.pptx'):
+            pptx_path = os.path.join(root, file)
+            txt_path = os.path.splitext(pptx_path)[0] + '.txt'
+
+            # Open the PowerPoint presentation
+            presentation = powerpoint.Presentations.Open(pptx_path)
+
+            # Initialize a variable to store text from all slides
+            all_text = []
+
+            # Iterate through slides and extract text
+            for slide in presentation.Slides:
+                for shape in slide.Shapes:
+                    if shape.HasTextFrame:
+                        text_range = shape.TextFrame.TextRange
+                        if text_range.Text:
+                            all_text.append(text_range.Text)
+
+            # Save the extracted text to a text file
+            with open(txt_path, 'w', encoding='utf-8') as txt_file:
+                txt_file.write('\n'.join(all_text))
+
+            # Close the presentation
+            presentation.Close()
+
+            print(f'Converted {pptx_path} to {txt_path}')
+
+# Quit PowerPoint application
+powerpoint.Quit()
+
+
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        if file.endswith('.txt'):
+            txt_path = os.path.join(root, file)
+            output_txt_path = os.path.join(output_path, file)
+            shutil.move(txt_path, output_txt_path)
+            print(f'Moved {txt_path} to {output_txt_path}')
